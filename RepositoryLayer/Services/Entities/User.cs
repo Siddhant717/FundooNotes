75da55afdcee819c48fce546a6c83e0fa418756f
Siddhant <santoshshrivastava39@gmail.com>
2022-08-25 18:36:06 +0530

    Migration

---
+++ RepositoryLayer/Services/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Services.Entities
+{
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+
+    }
+}
